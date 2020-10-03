db4df34642bc2eb07dcc360df3856222e420ee42
Donnie <donnieashok@gmail.com>
2020-10-03 17:15:41 +0200

    A bit of styling

--- client/src/parts/commit-list.tsx
+++ client/src/parts/commit-list.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import { connect } from "react-redux";
 import { createSelector } from 'reselect';
+import styled from 'styled-components'
 import { Commit, CommitSearch } from '../types'
+
+const COMMIT = styled.div`
+  color: white;
+  margin: 25px 0 10px 10px;
+  display: flex;
+  line-height: 1.6;
+`
+
+const AUTHOR = styled.div`
+  margin: 0 15px 0 0;
+`
+
+const MESSAGE = styled.div`
+  text-align: justify;
+  text-justify: inter-word;
+`
 
 type Props = {
   commits: Commit[],
@@ -11,10 +28,11 @@ function CommitList({ commits }: Props) {
   return (
     <div>
       {
-        commits.map(commit => (
-          <div key={commit.date}>
-            {commit.message}
-          </div>
+        commits.map((commit, i) => (
+          <COMMIT key={commit.date}>
+            <AUTHOR>{`${commit.author_name} committed:`}</AUTHOR>
+            <MESSAGE>{commit.message}</MESSAGE>
+          </COMMIT>
         ))
       }
     </div>
--- client/src/parts/recent-list.tsx
+++ client/src/parts/recent-list.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import { Recent } from '../types'
+
+const RECENTLIST = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 80vw;
+`
+
+const RECENT = styled.div`
+  color: lightcyan;
+  cursor: pointer;
+  margin: 0 10px 20px 0;
+`
 
 type Props = {
   recents: Recent[],
@@ -9,13 +23,15 @@ export default function RecentList({ recents }: Props) {
   return (
     <div>
       <h3>Recent repositories: </h3>
-      {
-        recents.map(rec => (
-          <div>
-            {rec.repo_name}
-          </div>
-        ))
-      }
+      <RECENTLIST>
+        {
+          recents.map(rec => (
+            <Link to={`/${rec.repo_user}/${rec.repo_name}`} href="#" key={`${rec.repo_user}/${rec.repo_name}`}>
+              <RECENT>{rec.repo_name}</RECENT>
+            </Link>
+          ))
+        }
+      </RECENTLIST>
     </div>
   )
 }
